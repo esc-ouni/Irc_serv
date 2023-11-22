c708ca1305b21d9f6a14e70e5ca66874e37c7149
esc-ouni <idouni@student.1337.ma>
2023-11-22 13:55:29 +0100

    Irc

--- Irc_serv.cpp
+++ Irc_serv.cpp
@@ -6,14 +6,18 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:22:37 by idouni            #+#    #+#             */
-/*   Updated: 2023/11/22 11:50:51 by idouni           ###   ########.fr       */
+/*   Updated: 2023/11/22 13:55:03 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Irc_serv.hpp"
 
+void disconnect(int opened_socket){
+	close(opened_socket);
+};
+
 int run_server(int argc, char const *argv[]){
-	std::cout << "Loading ..." << std::endl;
+	std::cout << "LOADING ..." << std::endl;
 
 	if (argc != 2)
 		throw (std::logic_error("Bad Aruments, try : ./ircserv <port> \n"));
@@ -28,49 +32,61 @@ int run_server(int argc, char const *argv[]){
 
 	struct sockaddr_in *sockaddr_in = new struct sockaddr_in;
 
-
-	
 	sockaddr_in->sin_family = AF_INET;
 	sockaddr_in->sin_port = htons(port);
 	sockaddr_in->sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if (bind(sock_id, (const struct sockaddr *)sockaddr_in , sizeof(sockaddr)) == -1)
+	if (bind(sock_id, (const struct sockaddr *)sockaddr_in , sizeof(sockaddr)) == -1){
+		disconnect(sock_id);
 		throw (std::logic_error("Error Binding A Socket\n"));
+	}
 	
-
-	if (listen(sock_id, 1) == -1)
-		throw (std::logic_error("Error Listening On Socket\n"));
+	if (listen(sock_id, 1) == -1){
+		disconnect(sock_id);
+		throw (std::logic_error("Error Listening On Socket\n"));	
+	}
 
 	std::cout << "READY TO CONNECT ..." << std::endl;
 	int client = accept(sock_id, NULL, NULL);
-	if ( client == -1)
+	if ( client == -1){
+		disconnect(sock_id);
 		throw (std::logic_error("Error Accepting Request\n"));
-	
-	
+	}
+	disconnect(sock_id);
 	return (client);
 };
-
 
 void handel_client(int client){
 	std::cout << "Client 1, CONNECTED SUCCESSFULLY" << std::endl << std::endl;
 	char recieved[1024];
 	bzero(recieved, sizeof(recieved));
-	int readed = read(client, recieved, 1023);
+	int readed = recv(client, recieved, 1023, 0);
+	int msg_length = strlen(recieved);
+	
 	while (true){
-		if (readed > 0){
-			recieved[strlen(recieved) - 2] = '\0';
+		if (msg_length > 0){
+			recieved[strlen(recieved) - 1] = '\0';
 			std::cout << "Client 1 sent : [" << recieved <<"] " << std::endl;
 			bzero(recieved, sizeof(recieved));
 			readed = 0;
+			msg_length = 0;
 		}
-		else if (readed == -1)
-			throw (std::logic_error("Error Reading Request\n"));		
+		else if (readed == -1){
+			disconnect(client);
+			std::cout << std::endl;
+			throw (std::logic_error("Error During Waiting For Requests\n"));		
+		}
+		else if (!readed){
+			disconnect(client);
+			std::cout << std::endl;
+			throw (std::logic_error("CLIENT DISCONNECTED, ..."));		
+		}
 		bzero(recieved, sizeof(recieved));
-		readed = read(client, recieved, 1023);
+		readed = recv(client, recieved, 1023, 0);
+		recieved[strlen(recieved)] = '\0';
+		msg_length = strlen(recieved);
 	}
-	close(client);
 }
-
 
 int main(int argc, char const *argv[]){
 	int client;
