ff4680f3807ea662d46452b189dfa46b2f908c1e
esc-ouni <idouni@student.1337.ma>
2023-12-04 14:18:26 +0100

    MODE_PRGRS

--- src/commands/mode.cpp
+++ src/commands/mode.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mode.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:17:20 by idouni            #+#    #+#             */
+/*   Updated: 2023/12/04 14:17:48 by idouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../headers/Irc.hpp"
 #include "../../headers/Channel.hpp"
 #include "../../headers/commands.hpp"
@@ -8,4 +20,5 @@ void mode(std::string command, Client &client, std::map<std::string, Channel>& c
     std::cout << "channel  : <" << extract_channel_name(command) << "> " << std::endl;
     std::cout << "mode     : <" << extract_target(command) << "> " << std::endl;
     std::cout << "params   : <" << extract_reason(command) << "> " << std::endl;
+    //PARSIN WITH SS
 };
