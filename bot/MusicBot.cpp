81fba278cdc757a7e25bf1ff48b9465612abfaf7
esc-ouni <idouni@student.1337.ma>
2023-12-13 17:26:21 +0100

    /BOT_TBC

--- bot/MusicBot.cpp
+++ bot/MusicBot.cpp
@@ -1,102 +1,139 @@
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
-#include <iostream>
-#include <fcntl.h> // for fcntl
-#include <poll.h>  // for poll
-
-
 #include "../headers/Irc.hpp"
 #include "../headers/Channel.hpp"
 #include "../headers/commands.hpp"
+#include <netdb.h>
+
+long int contains_only_nums(std::string string){
+    if (string.empty())
+        return (-1);
+    for (size_t i = 0; i < string.length(); i++){
+        if (string.c_str()[0] == '+')
+            continue ;
+        if (!isdigit(string.c_str()[i]) || i == 10)
+            return (0);
+    }
+    return (std::atol(string.c_str()));
+};
 
 void send_message(int client_socket, std::string message) {
     if (send(client_socket, message.c_str(), message.length(), 0) == -1)
         std::cerr << "Err: failling sending message to the client !" << std::endl;
-    usleep(100);
+    usleep(300);
 };
 
 
-const char* server_ip = "127.0.0.1"; // Replace with your IRC server's IP address
-const int server_port = 6667;        // Replace with your IRC server's port
+int main(int argc, char *argv[]) {
+    if (argc != 3){
+        std::cerr << "Usage: <Ip> <port>!" << std::endl;
+        return 0;    
+    }
+    struct sockaddr_in addr;
+
+    struct pollfd pfd;
+
+    char buff[512];
 
 
-int main() {
-        std::string play;
+    bzero(&addr, sizeof(addr));
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Socket creation failed");
-        return -1;
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port        = htons(contains_only_nums(argv[2]));
+
+    int socket_end = socket(PF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);
+
+    if (connect(socket_end, (struct sockaddr *)&addr, sizeof(addr)) == -1){
+        std::cerr << "Err: failling to connect socket!" << std::endl;
+        return 0;
+    }
+    std::cout << "SOCKET CONNECTED SUCCESSFULLY !" << std::endl;
+    
+    pfd.fd     = socket_end;
+    pfd.events = POLLIN;
+
+    int ready_fd;
+
+    send_message(socket_end, "PASS pass\r\n");
+    send_message(socket_end, "NICK MusicBot\r\n");
+    send_message(socket_end, "USER 1 0 *\r\n");
+    send_message(socket_end, "JOIN #music\r\n");
+    send_message(socket_end, "MODE #music +k password1\r\n");
+    send_message(socket_end, "TOPIC #music :Use <!Bot dance> Command For The Music Of The Day\r\n");
+
+    
+    while (1){
+        if ((ready_fd = poll(&pfd, 1, 100)) > 0){
+            recv(socket_end, buff, 512, MSG_OOB);
+            buff[511] = '\0';
+            std::cout << buff << std::endl; 
+        }
+        bzero(buff, sizeof(buff));
     }
 
-    // Set socket to non-blocking mode
-    // int flags = fcntl(sock, F_GETFL, 0);
-    // if (flags == -1) {
-    //     perror("fcntl failed to get flags");
-    //     close(sock);
-    //     return -1;
-    // }
-    // if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
-    //     perror("fcntl failed to set non-blocking flag");
-    //     close(sock);
-    //     return -1;
-    // }
-
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-
-    // Connect to the IRC server
-    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        if (errno != EINPROGRESS) {
-            perror("Connect failed");
-            close(sock);
-            return -1;
-        }
-    }
-
-    // IRC protocol handshake
-    send_message(sock, "PASS pass\r\n");
-    send_message(sock, "NICK MusicBot\r\n");
-    send_message(sock, "USER 1 0 * \r\n");
-
-    // Join a channel
-    send_message(sock, "JOIN #music\r\n");
-
-    // Main loop to listen to messages from the server and respond
-    char buffer[512];
-    struct pollfd pfd = {sock, POLLIN, 0};
-    while (true) {
-        int poll_result = poll(&pfd, 1, -1); // Infinite timeout
-        if (poll_result == -1) {
-            perror("poll failed");
-            break;
-        }
-        if (poll_result > 0) {
-            ssize_t bytes_received = recv(sock, buffer, sizeof(buffer), 0);
-            if (bytes_received < 0) {
-                perror("Receive failed");
-                break;
-            }
-            buffer[bytes_received] = '\0';
-            std::cout << buffer << std::endl;
-
-            play = buffer;
-            if (play.find("!Bot dance") != std::string::npos){
-                play = "/usr/bin/afplay b_tmp/m.mp3 &";
-                std::system(play.c_str());
-                send_message(sock, "PRIVMSG #music : Now playing sba7 lkheir \r\n");
-            }
-            play.clear();
-            bzero(buffer, sizeof(buffer));
-        }
-    }
-
-    close(sock);
-    return 0;
+    return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // IRC protocol handshake
+    // send_message(sock, "PASS pass\r\n");
+    // send_message(sock, "NICK MusicBot\r\n");
+    // send_message(sock, "USER 1 0 * \r\n");
+    // send_message(sock, "JOIN #music\r\n");
+    //         play = buffer;
+    //         if (play.find("!Bot dance") != std::string::npos){
+    //             play = "/usr/bin/afplay b_tmp/m.mp3 &";
+    //             std::system(play.c_str());
+    //             send_message(sock, "PRIVMSG #music : Now playing sba7 lkheir \r\n");
+    //         }
+    //         play.clear();
+    //         bzero(buffer, sizeof(buffer));
