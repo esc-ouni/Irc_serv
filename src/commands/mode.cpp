f32d558989afa4bebd00edb4a6522068c83a833e
esc-ouni <idouni@student.1337.ma>
2023-12-15 13:40:06 +0100

    /RETOUCHES_(MODE)

--- src/commands/mode.cpp
+++ src/commands/mode.cpp
@@ -6,7 +6,7 @@
 /*   By: idouni <idouni@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 14:17:20 by idouni            #+#    #+#             */
-/*   Updated: 2023/12/14 20:13:21 by idouni           ###   ########.fr       */
+/*   Updated: 2023/12/15 13:36:45 by idouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ void mode_with_params(std::map<std::string, Channel>& channels, Client &excuter,
     int                      param_c = 0;
     
     if (!channel_exist(channels, channel_name)){
-            send_message(excuter.get_fd(), ERR_NOSUCHCHANNEL(excuter.get_nickname(), channel_name));
+        send_message(excuter.get_fd(), ERR_NOSUCHCHANNEL(excuter.get_nickname(), channel_name));
         return ;   
     }
     if (!excuter.is_operator(channels[channel_name]) && !excuter.is_IRC_op()){
@@ -89,19 +89,14 @@ void mode_with_params(std::map<std::string, Channel>& channels, Client &excuter,
 
     param  = mode_parser(last_param, ',');
     param_c = param.size();
-    for (int i = 0; i < param_c; i++){
-        // std::cout << " <" << param.at(i) << "> " << std::endl;
-    }
     
     for (int i = 1; i < mode.size(); i++){
         if (i <= param_c)
             s_param = param.at(i - 1);
         else
             s_param = "";
-        
         s_mode += mode.at(0);
         s_mode += mode.at(i);
-        // std::cout << std::endl << "mode : <" << s_mode << "> , param : <" << s_param << "> " << std::endl;
         if (!valid_option(s_mode)){
             send_message(excuter.get_fd(), ERR_UMODEUNKNOWNFLAG(excuter.get_nickname(), channel_name, s_mode.at(1)));
         }
@@ -112,92 +107,119 @@ void mode_with_params(std::map<std::string, Channel>& channels, Client &excuter,
     }
 };
 
+void mode_flag_i(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode){
+    if (mode.at(0) == '+' && !channels[channel_name].get_option_i()){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+i", ""));
+        channels[channel_name].set_option_i(true);
+        return ;
+    }
+    if (mode.at(0) == '-' && channels[channel_name].get_option_i()){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-i", ""));
+        channels[channel_name].set_option_i(false);
+        return ;
+    }  
+    return ;
+};
 
-void execute_mode(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode, std::map<int, Client> &clients, std::string &last_param){
+void mode_flag_t(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode){
+    if (mode.at(0) == '+' && !channels[channel_name].get_option_t()){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+t", ""));
+        channels[channel_name].set_option_t(true);
+        return ;
+    }
+    else if (mode.at(0) == '-' && channels[channel_name].get_option_t()){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-t", ""));
+        channels[channel_name].set_option_t(false);
+        return ;
+    }
+    return ;
+};
 
-    if (mode.at(1) == 'i'){
-        if (mode.at(0) == '+' && !channels[channel_name].get_option_i()){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+i", ""));
-            channels[channel_name].set_option_i(true);
-            return ;
-        }
-        if (mode.at(0) == '-' && channels[channel_name].get_option_i()){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-i", ""));
-            channels[channel_name].set_option_i(false);
-            return ;
+void mode_flag_l(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode, std::string &last_param){
+    if (mode.at(0) == '+' && contains_only_nums(last_param) != channels[channel_name].get_limit()){
+        if (contains_only_nums(last_param) != -1){
+            if (channels[channel_name].set_limit(contains_only_nums(last_param))){
+                channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+l", last_param));
+                channels[channel_name].set_option_l(true);
+            }
         }
         return ;
     }
-    else if (mode.at(1) == 't'){
-        if (mode.at(0) == '+' && !channels[channel_name].get_option_t()){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+t", ""));
-            channels[channel_name].set_option_t(true);
-            return ;
-        }
-        else if (mode.at(0) == '-' && channels[channel_name].get_option_t()){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-t", ""));
-            channels[channel_name].set_option_t(false);
-            return ;
-        }
-        return ;
-    }
-    if (mode.at(1) == 'l' && mode.at(0) == '-' && channels[channel_name].get_option_l()){
+    if (mode.at(0) == '-' && channels[channel_name].get_option_l()){
         channels[channel_name].set_limit(CHANNEL_LIMIT);
         channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-l", ""));
         channels[channel_name].set_option_l(false);
         return ;
     }
-    
-    if (mode.at(1) == 'k'){
-        if (mode.at(0) == '+' && !channels[channel_name].get_option_k()){
-            if (!is_valid_password(last_param)){
-                send_message(excuter.get_fd(), ERR_PASSINVALID(excuter.get_nickname()));
-                return ;
-            }
-            if (channels[channel_name].set_password(last_param)){
-                channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+k", ""));
-                channels[channel_name].set_option_k(true);                
-                channels[channel_name].lock();
-            }
+    return ;
+};
+
+void mode_flag_k(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode, std::string &last_param){
+    if (mode.at(0) == '+' && !channels[channel_name].get_option_k()){
+        if (!is_valid_password(last_param)){
+            send_message(excuter.get_fd(), ERR_PASSINVALID(excuter.get_nickname()));
             return ;
         }
-        else if (mode.at(0) == '-' && channels[channel_name].get_option_k()){
-            if (last_param == channels[channel_name].get_password()){
-                channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-k", ""));
-                channels[channel_name].set_option_k(false);
-                channels[channel_name].unlock();
-            }
-            else
-                send_message(excuter.get_fd(), ERR_PASSWDMISMATCH(excuter.get_nickname()));
-            return ;
+        if (channels[channel_name].set_password(last_param)){
+            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+k", ""));
+            channels[channel_name].set_option_k(true);                
+            channels[channel_name].lock();
         }
         return ;
     }
-    else if (mode.at(1) == 'l'){
-        if (mode.at(0) == '+' && contains_only_nums(last_param) != channels[channel_name].get_limit()){
-            if (contains_only_nums(last_param) != -1){
-                if (channels[channel_name].set_limit(contains_only_nums(last_param))){
-                    channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+l", last_param));
-                    channels[channel_name].set_option_l(true);
-                }
-            }
+    else if (mode.at(0) == '-' && channels[channel_name].get_option_k()){
+        if (last_param == channels[channel_name].get_password()){
+            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-k", ""));
+            channels[channel_name].set_option_k(false);
+            channels[channel_name].unlock();
         }
+        else
+            send_message(excuter.get_fd(), ERR_PASSWDMISMATCH(excuter.get_nickname()));
+        return ;
+    }
+    return ;
+};
+
+void mode_flag_o(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode, std::map<int, Client> &clients, std::string &last_param){
+    if (!channels[channel_name].is_member(last_param)){
+        if (!last_param.empty())
+            send_message(excuter.get_fd(), ERR_USERNOTINCHANNEL(excuter.get_nickname(), last_param, channel_name));
+        return ;
+    }
+    if (mode.at(0) == '-' && channels[channel_name].is_operator(last_param)){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-o", last_param));
+        channels[channel_name].unpromote(channels[channel_name].is_member(last_param));         
+        return ;
+    }
+    if (mode.at(0) == '+' && !channels[channel_name].is_operator(last_param)){
+        channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+o", last_param));
+        channels[channel_name].promote(clients[channels[channel_name].is_member(last_param)]);         
+        return ;
+    }
+    return ;
+};
+
+void execute_mode(std::map<std::string, Channel>& channels, Client &excuter, std::string &channel_name, std::string &mode, std::map<int, Client> &clients, std::string &last_param){
+
+    if (mode.at(1) == 'i'){
+        mode_flag_i(channels, excuter, channel_name, mode);
+        return ;
+    }
+    if (mode.at(1) == 't'){
+        mode_flag_t(channels, excuter, channel_name, mode);
+        return ;
+    }
+    if (mode.at(1) == 'l'){
+        mode_flag_l(channels, excuter, channel_name, mode, last_param);
+        return ;
+    }
+    if (mode.at(1) == 'k'){
+        mode_flag_k(channels, excuter, channel_name, mode, last_param);
         return ;
     }
     if (mode.at(1) == 'o'){
-        if (!channels[channel_name].is_member(last_param)){
-            send_message(excuter.get_fd(), ERR_USERNOTINCHANNEL(excuter.get_nickname(), last_param, channel_name));
-            return ;
-        }
-        if (mode.at(0) == '-' && channels[channel_name].is_operator(last_param)){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "-o", last_param));
-            channels[channel_name].unpromote(channels[channel_name].is_member(last_param));         
-            return ;
-        }
-        if (mode.at(0) == '+' && !channels[channel_name].is_operator(last_param)){
-            channels[channel_name].broadcast_message(MODE_CHANGED(excuter.get_nickname(), excuter.get_client_host(), channel_name, "+o", last_param));
-            channels[channel_name].promote(clients[channels[channel_name].is_member(last_param)]);         
-            return ;
-        }
+        mode_flag_o(channels, excuter, channel_name, mode, clients, last_param);
+        return ;
     }
+    return ;
 };
